2075c0db9cc1d716e00e67ae7cf5fea9f295bf50
sorss <kongchiping9@gmail.com>
2026-02-21 18:25:48 +0900

    Add files via upload

---
+++ sample.sh
@@ -0,0 +1,8 @@
+gaster pwn && gaster reset
+irecovery -f iBSS.img4
+irecovery -f iBSS.img4
+irecovery -f iBEC.img4
+irecovery -f devicetree.img4
+irecovery -c devicetree
+irecovery -f kernelcache.img4
+irecovery -c bootx
