db994bd71d29e1e68de9147f05e512a3a879e038
sorss <kongchiping9@gmail.com>
2026-02-21 18:26:33 +0900

    Delete 7boot5s directory

--- 7boot5s/sample.sh
+++
@@ -1,8 +0,0 @@
-gaster pwn && gaster reset
-irecovery -f iBSS.img4
-irecovery -f iBSS.img4
-irecovery -f iBEC.img4
-irecovery -f devicetree.img4
-irecovery -c devicetree
-irecovery -f kernelcache.img4
-irecovery -c bootx
